b36fd5a582235e354cf37c63f0e6e081d0cd640e
indmind <sattanationmail@gmail.com>
2020-03-12 14:51:05 +0700

    build

---
+++ docs/precache-manifest.67b6104062f333dd84dff4351422e8c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e0169e499975e2358b",
+    "url": "/TodoPWA/css/app.6be1056d.css"
+  },
+  {
+    "revision": "efe451ae8abcd3c96a9fde2c07e02624",
+    "url": "/TodoPWA/index.html"
+  },
+  {
+    "revision": "97e0169e499975e2358b",
+    "url": "/TodoPWA/js/app.a31800e2.js"
+  },
+  {
+    "revision": "a06fb4345afa7e8c0fc9",
+    "url": "/TodoPWA/js/chunk-vendors.ce8f3594.js"
+  },
+  {
+    "revision": "b4f557a3eb98ba5fa3ceaec99408dee6",
+    "url": "/TodoPWA/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TodoPWA/robots.txt"
+  }
+]);
